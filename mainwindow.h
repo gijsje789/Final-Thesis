05d00fd97e95872584040f282d98e7f6b3d09630
Gijs de Vries <gijsjaapdevries@hotmail.com>
2019-02-07 14:48:54 +0100

    Sensor 1, all widgets

--- mainwindow.h
+++ mainwindow.h
@@ -5,7 +5,13 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QComboBox>
+#include <QRadioButton>
+#include <QCheckBox>
+
 #include <QGridLayout>
+#include <QVBoxLayout>
+
+#include <QDoubleValidator>
 
 class MainWindow : public QMainWindow
 {
@@ -17,7 +23,23 @@ public:
 
 private:
     QWidget *centralWidget = new QWidget;
-    QGridLayout *analogue1 = new QGridLayout;
+    QWidget *an1_widget = new QWidget(centralWidget);
+    QGridLayout *an1_gridLayout = new QGridLayout(an1_widget);
+    QVBoxLayout *an1_radio_vBox = new QVBoxLayout;
+
+    QLabel *an1_a_label = new QLabel;
+    QLineEdit *an1_a_val_lineEdit = new QLineEdit;
+    QComboBox *an1_a_unit_comboBox = new QComboBox;
+
+    QLabel *an1_b_label = new QLabel;
+    QLineEdit *an1_b_val_lineEdit = new QLineEdit;
+    QComboBox *an1_b_unit_comboBox = new QComboBox;
+
+    QRadioButton *an1_flow_radioButton = new QRadioButton;
+    QRadioButton *an1_pres_radioButton = new QRadioButton;
+
+    QCheckBox *an1_enable_checkBox = new QCheckBox;
+
 };
 
 #endif // MAINWINDOW_H
