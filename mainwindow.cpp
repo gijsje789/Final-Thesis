74adb057cccc4dea98135c4ee52301f3aa077c1e
Gijs de Vries <gijsjaapdevries@hotmail.com>
2019-02-07 16:37:53 +0100

    Added all tabs for 5 analogue sensors. No added functionality.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,12 +10,15 @@ MainWindow::MainWindow(QWidget *parent)
     this->setCentralWidget(centralWidget);
 
     // Create widget for analogue sensor 1.
-
-    an1_widget->setLayout(an1_menu->getMainLayout());
+    //an1_widget->setLayout(an1_menu->getMainLayout());
 
     // Create tab widget and populate with analogue sensors.
     anSensors_tabWidget->setGeometry(10, 10, 984/3, 150);
     anSensors_tabWidget->addTab(an1_widget, "AN1");
+    anSensors_tabWidget->addTab(an2_widget, "AN2");
+    anSensors_tabWidget->addTab(an3_widget, "AN3");
+    anSensors_tabWidget->addTab(an4_widget, "AN4");
+    anSensors_tabWidget->addTab(an5_widget, "AN5");
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -18,7 +18,15 @@ private:
     QWidget *centralWidget = new QWidget;
     QTabWidget *anSensors_tabWidget = new QTabWidget(centralWidget);
     QWidget *an1_widget = new QWidget;
+    QWidget *an2_widget = new QWidget;
+    QWidget *an3_widget = new QWidget;
+    QWidget *an4_widget = new QWidget;
+    QWidget *an5_widget = new QWidget;
     AnalogueSensorMenu *an1_menu = new AnalogueSensorMenu(an1_widget);
+    AnalogueSensorMenu *an2_menu = new AnalogueSensorMenu(an2_widget);
+    AnalogueSensorMenu *an3_menu = new AnalogueSensorMenu(an3_widget);
+    AnalogueSensorMenu *an4_menu = new AnalogueSensorMenu(an4_widget);
+    AnalogueSensorMenu *an5_menu = new AnalogueSensorMenu(an5_widget);
 };
 
 #endif // MAINWINDOW_H
